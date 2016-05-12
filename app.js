fda744479f91804add5fc43064df02a7c50134ea
Drew Robbins <drew@drewby.com>
2016-05-12 11:07:30 +0900

    Added capability to generate random sequence of messages for testing/demo

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var randomString = require('make-random-string')
 
 app.use(express.static('static'));
 
@@ -9,6 +10,13 @@ app.get('/msg/:guid', function (req, res) {
   console.log('MSG: ' + req.url);
   io.emit('msg', req.params.guid );
   res.status(200).send({ message: 'Message accepted' });
+});
+
+app.get('/rnd', function (req, res) {
+  console.log('MSG: ' + req.url);
+  io.emit('msg', req.params.guid );
+  res.status(200).send({ message: 'Message accepted' });
+  randomData();
 });
 
 io.on('connection', function(socket){
@@ -23,3 +31,13 @@ http.listen(port, function () {
   console.log('Listening on port ' + port);
 });
 
+var count = 0;
+function randomData() {
+  io.emit('msg', randomString(100));
+  count++;
+  if (count<1000) {
+    setTimeout(randomData, Math.random()*50);
+  } else {
+    count = 0;
+  }
+}
